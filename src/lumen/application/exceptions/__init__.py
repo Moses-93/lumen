4e80df43007596c08eaeadb21a7a2b258677f80f
Moses-93 <moiseenko2001@gmail.com>
2026-04-26 20:39:48 +0300

    feat(application): implement persistence-related exceptions

---
+++ src/lumen/application/exceptions/__init__.py
@@ -0,0 +1,26 @@
+from .persistence import (
+    PersistenceConfigurationError,
+    PersistenceAuthError,
+    PersistenceConnectionError,
+    PersistenceConstraintError,
+    PersistenceInvalidDataError,
+    PersistenceOperationalError,
+    PersistenceQueryError,
+    PersistenceReplyError,
+    PersistenceTimeoutError,
+    PersistenceError,
+)
+
+
+__all__ = [
+    "PersistenceConfigurationError",
+    "PersistenceAuthError",
+    "PersistenceConnectionError",
+    "PersistenceConstraintError",
+    "PersistenceInvalidDataError",
+    "PersistenceOperationalError",
+    "PersistenceQueryError",
+    "PersistenceReplyError",
+    "PersistenceTimeoutError",
+    "PersistenceError",
+]
