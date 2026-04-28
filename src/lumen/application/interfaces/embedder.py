82ffad335226a1fc8369a818703285b08c1d5e9d
Moses-93 <moiseenko2001@gmail.com>
2026-04-28 17:38:35 +0300

    feat(application): introduce embed_many to  Embedder protocol

--- src/lumen/application/interfaces/embedder.py
+++ src/lumen/application/interfaces/embedder.py
@@ -1,5 +1,9 @@
-from typing import Protocol
+from typing import Protocol, Iterable
 
 
 class Embedder(Protocol):
     def embed(self, text: str) -> list[float]: ...
+
+    def embed_many(
+        self, texts: Iterable[str], batch_size: int = 256
+    ) -> Iterable[list[float]]: ...
