7eca578c2c22c97af5f4bf6f224d7ffa9ed8208d
Moses-93 <moiseenko2001@gmail.com>
2026-04-28 18:55:07 +0300

    feat(application): define LlmClient protocol

--- src/lumen/application/interfaces/__init__.py
+++ src/lumen/application/interfaces/__init__.py
@@ -1,5 +1,5 @@
 from .embedder import Embedder
 from .unit_of_work import Session, Transaction, UnitOfWork
+from .llm_client import LlmClient
 
-
-__all__ = ["Embedder", "Session", "Transaction", "UnitOfWork"]
+__all__ = ["Embedder", "LlmClient", "Session", "Transaction", "UnitOfWork"]
